b57ece90cf367edfc79fe59fd875d7d19d4a52d7
Your Name <your-email@example.com>
2025-04-01 13:32:00 +0330

    Fix config and add docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // For root-relative paths
   plugins: [react()],
+  build: {
+    outDir: 'docs'  // GitHub Pages looks for this folder
+  }
 })
